be28860af605b4873ef62ee1b4e69eb97dae6156
William Patty <wpatty24@gmail.com>
2011-11-09 14:32:59 -0600

    Added Event NSManagedObject Creation

--- PSIdea/PSIdea/Event.m
+++ PSIdea/PSIdea/Event.m
@@ -30,7 +30,7 @@
     NSFetchRequest *request = [[NSFetchRequest alloc] init];
     
     request.entity =[NSEntityDescription entityForName:@"Event" inManagedObjectContext:context];
-    request.predicate = [NSPredicate predicateWithFormat:@"id = %@", idNumber];
+    request.predicate = [NSPredicate predicateWithFormat:@"idNumber = %@", idNumber];
     
     NSError *error = nil;
     
@@ -44,20 +44,45 @@
         event.latitude = latitude;
         event.longitude = longitude;
         event.public =public;
+        //TO DO: set up photo
         
-        //TO DO: set up event and photo
+        User *user = nil;
+        NSFetchRequest *request = [[NSFetchRequest alloc] init];
         
+        request.entity =[NSEntityDescription entityForName:@"User" inManagedObjectContext:context];
+        request.predicate = [NSPredicate predicateWithFormat:@"idNumber = %@", creator];
+        
+        NSError *error = nil;
+        
+        user = [[context executeFetchRequest:request error:&error] lastObject];
+        if(!error && !user){
+            user = [User createUserWithID:creator andName:nil andLatitude:nil andLongitude:nil andEvents:nil andFriends:nil andPhoto:nil inManagedObjectContext:context];
+        }
+        event.creator = user;
         
     }
     else if(!error && event){
-        if(event.title==@"null"){
+        if(event.title==nil){
             event.title = title;
             event.details = details;
             event.latitude = latitude;
             event.longitude = longitude;
             event.public =public;
             
-            //TO DO: set up event and photo
+            //TO DO: set up photo
+            User *user = nil;
+            NSFetchRequest *request = [[NSFetchRequest alloc] init];
+            
+            request.entity =[NSEntityDescription entityForName:@"User" inManagedObjectContext:context];
+            request.predicate = [NSPredicate predicateWithFormat:@"idNumber = %@", creator];
+            
+            NSError *error = nil;
+            
+            user = [[context executeFetchRequest:request error:&error] lastObject];
+            if(!error && !user){
+                user = [User createUserWithID:creator andName:nil andLatitude:nil andLongitude:nil andEvents:nil andFriends:nil andPhoto:nil inManagedObjectContext:context];
+            }
+            event.creator = user;
         }
         
         
